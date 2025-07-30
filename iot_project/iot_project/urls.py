1d1ca0df2cc23b9745bdf54a301bc6fd60aaa42f
Underwater-Drone-project <bhusarevaibhav91@gmail.com>
2025-07-31 00:10:04 +0530

    added core/form.py and update arduino code

--- iot_project/iot_project/urls.py
+++ iot_project/iot_project/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import homepage, register_user, login_user, logout_user, device_onboarding_view, add_device_to_user
 
+# For serving media files during development
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
@@ -9,11 +13,13 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
 
-    # Public facing device onboarding (before login/register)
     path('device-setup/', device_onboarding_view, name='device_onboarding'),
-    # Page after login/register to associate device
     path('add-device/', add_device_to_user, name='add_device_to_user'),
 
-    path('api/v1/device/', include('device_api.urls')), # Device API endpoints
-    path('dashboard/', include('dashboard.urls')),     # User dashboard
+    path('api/v1/device/', include('device_api.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
+
+# ONLY add this for development to serve media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
